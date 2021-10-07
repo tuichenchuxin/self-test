8104ac9c2eabccc15b5ca014cdb212ccbf801cd0
tuichenchuxin <chuxinchen.x@qq.com>
2021-10-07 23:42:35 +0800

    add oracle properties

--- src/main/java/com/example/org/shardingjdbcmetadatatest/ShardingJdbcMetaDataTestApplication.java
+++ src/main/java/com/example/org/shardingjdbcmetadatatest/ShardingJdbcMetaDataTestApplication.java
@@ -1,20 +1,69 @@
 package com.example.org.shardingjdbcmetadatatest;
 
-import org.apache.shardingsphere.driver.jdbc.core.datasource.ShardingSphereDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 
 @SpringBootApplication
+@RestController
 public class ShardingJdbcMetaDataTestApplication {
-
-	public static void main(String[] args) throws InterruptedException {
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(ShardingJdbcMetaDataTestApplication.class, args);
-		applicationContext.getBean(ShardingSphereDataSource.class);
-		while(true) {
-			Thread.sleep(1000);
-		}
-	}
-
+    
+    @Autowired
+    @Qualifier(value = "shardingSphereDataSource")
+    private DataSource dataSource;
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcMetaDataTestApplication.class, args);
+    }
+    
+    @GetMapping("/hello")
+    public String sql(@RequestParam(value = "sql") String sql) {
+        StringBuffer stringBuffer1 = new StringBuffer();
+        StringBuffer stringBuffer2 = new StringBuffer();
+        try {
+            try (
+                    Connection conn = dataSource.getConnection();
+                    PreparedStatement ps = conn.prepareStatement(sql)) {
+                
+                try {
+                    if (sql.toLowerCase().startsWith("select")) {
+                        ResultSet rs = ps.executeQuery();
+                        ResultSetMetaData metaData = rs.getMetaData();
+                        
+                        for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                            String columnName = metaData.getColumnName(i);
+                            stringBuffer1.append("--").append(columnName);
+                        }
+                        while (rs.next()) {
+                            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                                Object value = rs.getObject(i);
+                                stringBuffer2.append("--").append(value);
+                            }
+                        }
+                        
+                    } else {
+                        ps.execute();
+                    }
+                } finally {
+                
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+        return stringBuffer1.length() == 0 ? "success" : stringBuffer1.append("\n").append(stringBuffer2).toString();
+    }
+    
 }
